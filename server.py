a20db29ac2d1cdae18b7f6ee660dad4db31f4636
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-25 18:02:01 -0700

    complete through handling login & logout

--- server.py
+++ server.py
@@ -27,6 +27,84 @@ def index():
     return render_template("homepage.html")
 
 
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Register new user"""
+    
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Register new user"""
+
+    login = request.form.get("login")
+    password = request.form.get("password")
+    new_user = db.session.query(User).filter(User.email==login).first()
+    if new_user:
+        # user already exists
+        return redirect("/register")
+    else:
+        # add user to db
+        user = User(email=login,password=password)
+        db.session.add(user)
+        db.session.commit()
+
+    return redirect("/")
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Show login form."""
+
+    # if session["logged_in_customer_email"] is None:
+    return render_template("login_form.html")
+    # else:
+    #     flash("You are already logged in.")
+    #     return redirect("/")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Log user into site.
+
+    Find the user's login credentials located in the database,
+    look up the user, and store them in the session.
+    """
+
+    email = request.form.get('login')
+    password = request.form.get('password')
+
+    user = db.session.query(User).filter(User.email==email).first()
+
+    if not user:
+        flash("No such email address.")
+        return redirect('/login')
+
+    if user.password != password:
+        flash("Incorrect password.")
+        return redirect("/login")
+
+    session["logged_in_customer_email"] = user.email
+    flash("Logged in.")
+    return redirect("/")
+
+@app.route("/logout")
+def process_logout():
+    """Log user out."""
+
+    del session["logged_in_customer_email"]
+    flash("Logged out.")
+    return redirect("/")
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
@@ -35,7 +113,7 @@ if __name__ == "__main__":
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
 
     
